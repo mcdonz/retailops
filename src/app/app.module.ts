d7bd2adb64ec34c84af08b523c45f30115bad0fe
Donald Gatan <donaldgatan@Donalds-MacBook-Pro.local>
2020-11-08 00:29:53 +0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,51 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+import {MatButtonModule} from '@angular/material/button';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { DataService } from './services/data.service';
+import { AddDialogComponent } from './dialogs/add/add-dialog/add-dialog.component';
+import { EditDialogComponent } from './dialogs/edit/edit-dialog/edit-dialog.component';
+import { DeleteDialogComponent } from './dialogs/delete/delete-dialog/delete-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    AddDialogComponent,
+    EditDialogComponent,
+    DeleteDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatIconModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatListModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
